2afc3679beecf12943028a68fecb03dac90e2a29
ammarshahin <ammar0ammar612@gmail.com>
2019-11-24 17:46:58 +0200

    Initilizing the Repo

---
+++ SOS/SOS/main.c
@@ -0,0 +1,18 @@
+/*
+ * SOS.c
+ *
+ * Created: 11/24/2019 5:45:46 PM
+ * Author : AVE-LAP-44
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
